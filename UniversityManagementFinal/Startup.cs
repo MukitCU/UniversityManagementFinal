129e8564d8d24a238aa7877efb8113ee66a34043
Mukit <Mukit@DESKTOP-ST0BC03>
2016-08-18 16:33:13 +0600

    Add project files.

---
+++ UniversityManagementFinal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UniversityManagementFinal.Startup))]
+namespace UniversityManagementFinal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
